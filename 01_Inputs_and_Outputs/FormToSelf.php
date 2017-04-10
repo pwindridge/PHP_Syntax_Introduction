4c144cc32e54c057a9b802228959614f2aa47829
Philip Windridge <p.c.windridge@staffs.ac.uk>
2017-04-10 08:15:29 +0100

    Added FormToSelf.php to 01_Inputs_and_Outputs

---
+++ 01_Inputs_and_Outputs/FormToSelf.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form To Self</title>
+</head>
+<body>
+<form method="post">
+    <p>
+        <label for="name">Name: </label>
+        <input type="text" id="name" name="name">
+    </p>
+    <p><input type="submit" value="Submit"></p>
+</form>
+</body>
+</html>
